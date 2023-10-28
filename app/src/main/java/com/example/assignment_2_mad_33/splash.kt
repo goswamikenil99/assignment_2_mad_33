3f3c5738a9440cc15487ac76d607e01427573a69
kenil99 <goswamikenil9@gmail.com>
2023-10-29 01:17:58 +0530

    1.3

--- app/src/main/java/com/example/assignment_2_mad_33/splash.kt
+++ app/src/main/java/com/example/assignment_2_mad_33/splash.kt
@@ -4,16 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import com.google.android.material.animation.AnimationUtils
+import com.google.android.material.card.MaterialCardView
 
 class splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val animFadeOut=android.view.animation.AnimationUtils.loadAnimation(applicationContext,R.anim.animation);
+        val m:MaterialCardView=findViewById(R.id.m);
+        m.startAnimation(animFadeOut);
         Handler().postDelayed(
             {
                 Intent(this,login::class.java).also { startActivity(it) }
                 finish()
-            },3000
+            },2500
         )
     }
 }
